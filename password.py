1b09d3c8d4c366a7f21c276baad102b218fa311b
Shawn chiu <shawn20111207@gmail.com>
2024-08-04 10:31:59 +0800

    change while remain_time >= 0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 password = 'a123'
 remain_time = 3
-while True:
+while remain_time >= 1:
     pwd = input('請輸入密碼: ')
     if pwd == password:
             print('輸入成功')
@@ -8,10 +8,10 @@ while True:
     else:
         remain_time = remain_time - 1
         print('輸入錯誤! 還有',remain_time, '次機會')
-        if remain_time == 0:
-            print('因輸入多次錯誤，已無機會')
-            break
+if remain_time == 0:
+    print('輸入過多次錯誤 已取消輸入資格')
 
-    
+
+
    
 
